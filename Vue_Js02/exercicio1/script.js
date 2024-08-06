6b142eab752589fd76cab81e9451ad4f069d4260
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-06 19:13:57 -0300

    Atualizando a OT 02 de VueJS

--- Vue_Js02/exercicio1/script.js
+++ Vue_Js02/exercicio1/script.js
@@ -1,13 +1,20 @@
 // var app = new Vue({
-//     el: '#app',
+//     el: '#app',                      <!-- v-bind -->
 //     data: {
 //         url: 'https://vuejs.org'
+//     }
+// })
+
+// var app = new Vue({
+//     el: '#app',     Exemplo de  v-model 
+//     data: {
+//         message: ''
 //     }
 // })
 
 var app = new Vue({
     el: '#app',
     data: {
-        message: ''
+        seen: true
     }
 })
