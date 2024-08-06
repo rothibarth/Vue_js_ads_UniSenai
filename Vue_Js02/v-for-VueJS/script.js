72c97123c464c18d7cae17692cf8a5c1d0b54c2c
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-06 20:55:21 -0300

    Atualizando a OT 02 de Vue JS

---
+++ Vue_Js02/v-for-VueJS/script.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            {id: 1 , text: 'Item 1'},
+            {id: 2 , text: 'Item 2'},
+            {id: 3 , text: 'Item 3'},
+        ]
+    }
+})
