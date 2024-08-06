1dbd13c35095f09c109488831e777f6e18777e26
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2024-08-05 21:34:39 -0300

    Atualizando a OT de VueJs

---
+++ Vue_Js02/exercicio1/script.js
@@ -0,0 +1,13 @@
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         url: 'https://vuejs.org'
+//     }
+// })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: ''
+    }
+})
